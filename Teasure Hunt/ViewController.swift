4760622939c96c516a293dc560119a6f30aab59b
Eduard Guerra <eaguerra@students.tulsatech.edu>
2019-11-25 10:43:55 -0600

    Added close button and map type functionalities

--- Teasure Hunt/ViewController.swift
+++ Teasure Hunt/ViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 import CoreLocation
+import AVFoundation
+import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
+    //Var and Funcs
     var locationManager: CLLocationManager?
+    
+    //IBActions and IBOutlets
+    @IBAction func closeMapButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func mapChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 || sender.selectedSegmentIndex == 1 || sender.selectedSegmentIndex == 2 {
+          mapView.isHidden = false
+          if sender.selectedSegmentIndex == 0 {
+            mapView.mapType = .standard
+          } else if sender.selectedSegmentIndex == 1 {
+            mapView.mapType = .satellite
+          } else if sender.selectedSegmentIndex == 2 {
+            mapView.mapType = .hybrid
+            }
+        } else {
+          mapView.isHidden = true
+        }
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
